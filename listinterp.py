e75f1a8cbc2ffd4c0cbfd7c6c1cc9423e6ccd386
oliverwarne <oliverswarne@gmail.com>
2015-03-24 15:59:38 +0000

    again, shortened the function even more by using dicts

--- listinterp.py
+++ listinterp.py
@@ -1,46 +1,16 @@
 __name__ = "listinterp"
 
-
-def PeekArray(Scouted_Array,Data):
+def PeekArrayTest(Scouted_Array,Data):
     # You pass this function the 
-    TeamNum = Scouted_Array[0]
-    TeamName = Scouted_Array[1]
-    TeamTotesStacked = Scouted_Array[2]
-    TeamBinOnTotes = Scouted_Array[3]
-    TeamLitter = Scouted_Array[4]
-    TeamChute = Scouted_Array[5]
-    TeamMatchNum = Scouted_Array[6]
-
     IndexDict = {
-                'TeamNum':TeamNum,
-                'TeamName':TeamName,
-                'TeamTotesStacked':TeamTotesStacked,
-                'TeamBinOnTotes':TeamBinOnTotes,
-                'TeamLitter':TeamLitter,
-                'TeamChute':TeamChute,
-                'TeamMatchNum':TeamMatchNum
+                'TeamNum':Scouted_Array[0],
+                'TeamName':Scouted_Array[1],
+                'TeamTotesStacked':Scouted_Array[2],
+                'TeamBinOnTotes':Scouted_Array[3],
+                'TeamLitter':Scouted_Array[4],
+                'TeamChute':Scouted_Array[5],
+                'TeamMatchNum':Scouted_Array[6]
                 }
     
     return IndexDict[Data]
 
-def PeekArrayHACK(Scouted_Array,Data):
-    # This hack turns the string "False" and "True" into real bools to be passed
-    TeamNum = Scouted_Array[0]
-    TeamName = Scouted_Array[1]
-    TeamTotesStacked = Scouted_Array[2]
-    TeamBinOnTotes = Scouted_Array[3]
-    TeamLitter = bool(Scouted_Array[4])
-    TeamChute = bool(Scouted_Array[5])
-    TeamMatchNum = Scouted_Array[6]
-    
-    IndexDict = {
-                'TeamNum':TeamNum,
-                'TeamName':TeamName,
-                'TeamTotesStacked':TeamTotesStacked,
-                'TeamBinOnTotes':TeamBinOnTotes,
-                'TeamLitter':TeamLitter,
-                'TeamChute':TeamChute,
-                'TeamMatchNum':TeamMatchNum
-                }
-    
-    return IndexDict[Data]

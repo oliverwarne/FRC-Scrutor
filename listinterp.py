bd5ec8ac0fa1db2eefbfb363435f75deaa44d220
oliverwarne <oliverswarne@gmail.com>
2015-03-25 20:40:39 +0000

    added comments and shortened where possible

--- listinterp.py
+++ listinterp.py
@@ -1,7 +1,8 @@
 __name__ = "listinterp"
 
-def PeekArrayTest(Scouted_Array,Data):
-    # You pass this function the 
+def PeekArrayTest(Scouted_Array,Datatype):
+    # This dictionary lets you pass an array and Data (the type of data you want), and it'll return the 
+    # data from the array that you want.
     IndexDict = {
                 'TeamNum':Scouted_Array[0],
                 'TeamName':Scouted_Array[1],
@@ -11,6 +12,4 @@ def PeekArrayTest(Scouted_Array,Data):
                 'TeamChute':Scouted_Array[5],
                 'TeamMatchNum':Scouted_Array[6]
                 }
-    
-    return IndexDict[Data]
-
+    return IndexDict[Datatype]

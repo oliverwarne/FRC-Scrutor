44b0dfff59a30e83c98f6e60fc781bade03d0d9c
oliverwarne <oliverswarne@gmail.com>
2015-03-26 23:09:40 +0000

    removed unnessecary global var declerations

--- config.py
+++ config.py
@@ -5,34 +5,24 @@
 ### CUSTOMIZATION FOR ANALYZATION.py BEGIN ###
 
 # Miniumum of stuff
-global MinTotes
 MinTotes = 1
-global MinBins
 MinBins = 1
 
 # Bools for checking
-global MinTotesBool
 MinTotesBool = True
-global MinBinsOnToteBool
 MinBinsOnToteBool = True
-global NeedsLitter
 NeedsLitter = True
-global RankedHigherBool
 RankedHigherBool = True
-global MinimumScoreBool
 MinimumScoreBool = False
-global ChuteBool
 ChuteBool = True
 
 # Scoring stuff
-global TotesValue,BinValue,LitterValue,ChuteValue
 TotesValue = 2
 BinValue = 1
 LitterValue = 1
 ChuteValue = 1
 
 # Absolute Needs
-global ABSTotesPass,ABSBinPass,ABSLitterPass,ABSChutePass
 ABSTotesPass = True
 ABSBinPass = True
 ABSLitterPass = True
@@ -45,13 +35,16 @@ ABSChutePass = True
 # Miniumum of stuff
 MinTotes = 0
 MinBins = 4
+MinScore = 0
 
-# Bools for checking
-MinTotesBool = True
-MinBinsOnToteBool = True
-NeedsLitter = True
-RankedHigherBool = True
-MinimumScoreBool = False
+
+## Bools for checking
+#MinTotesBool = True
+#MinBinsOnToteBool = True
+#NeedsLitter = True
+#RankedHigherBool = True
+#MinimumScoreBool = False
+
 
 ### CUSTOMIZATION FOR PASSCHECK.py END ###
 

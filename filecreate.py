4451931b4abbfc80ab52678fe586cfc528907322
Oliver Warne <oliverswarne@gmail.com>
2015-03-28 22:14:33 +0000

    renamed file

---
+++ filecreate.py
@@ -0,0 +1,12 @@
+__name__ = "filecreate"
+
+### FILE LOADING STUFF BEGIN ###
+
+def GetNameAndTeamNum(Scouted_Array):
+    return str(Scouted_Array[0]) + " : " + str(Scouted_Array[1]) + ".txt"
+    
+def WriteToText(InfoArray):
+    file = open(
+    
+    
+### FILE LOADING STUFF END ###

8821471ed41357ac92986fafd3a206903c854c04
Oliver Warne <oliverswarne@gmail.com>
2015-03-29 03:43:49 +0000

    oops sorry past me, there is a way. but it's a hack so i still hate you

--- scan.py
+++ scan.py
@@ -3,13 +3,12 @@ import os
 import glob
 import time
 import array
-from ast import literal_eval
+import ast
 os.chdir("TestTexts/")
 scanned = open("scanned.txt","a")
 for file in glob.glob("*.txt"):
     f = open(file)
     for line in f:
-        line = line.split[1]
-        print analyzation.BoolArrayIntoTeamScore(line)
-        
-        
+        line = ast.literal_eval(line)
+        lineperc = analyzation.PercentageCheckAbsolute(line)
+        print lineperc

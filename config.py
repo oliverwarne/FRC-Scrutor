7ebe474fdef577c74fbd67a2771ecc8fd1d425bb
Oliver Warne <oliverswarne@gmail.com>
2015-04-15 17:09:43 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+textDir = "TestFolder/"
+
+heightMin = 3
+binMin = 4
+stacksMin = 1
+litterCheck = True
+alternateCheck = True
+bankrobCheck = True
+
+absHeight = True
+absBin = True
+absStacks = True
+absLitter = True
+
+heightValue = 1
+binValue = 1
+stacksValue = 1
+litterValue = 1
+bankrobValue = 1

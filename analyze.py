1a907f985f4eaa8e1f061bc8449d8bea012bf80c
Oliver Warne <oliverswarne@gmail.com>
2015-06-02 23:45:48 +0000

    past me, what were you doing? Current me rewrote everything

--- analyze.py
+++
@@ -1,9 +0,0 @@
-def boolPassFail(shouldCheck,checkBool):
-    if shouldCheck and checkBool:
-        return True
-    elif shouldCheck == False and checkBool:
-        return True
-    elif shouldCheck == checkBool:
-        return True
-    else:
-        return False
